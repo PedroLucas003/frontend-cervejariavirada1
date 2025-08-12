ff8ee21c4adae8310acc3bfda971a7c98fccdb63
PedroLucas003 <pedrolucasaraujo201733@gmail.com>
2025-08-12 14:35:56 -0300

    atualização da navbar2

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -15,7 +15,6 @@ const Navbar = ({ isAuthenticated, onLogout, user, cartItems }) => {
 
     const handleResize = () => {
       setIsMobile(window.innerWidth <= 768);
-      // Fecha o menu mobile se redimensionar para desktop
       if (window.innerWidth > 768) {
         setMobileMenuOpen(false);
       }
